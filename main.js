2e98be1244f22b496d8297eccbb6110c4a562c95
JohnnyBakaas <johnny@bakaas-cadconsult.no>
2022-10-19 20:39:00 +0200

    Framgang

--- main.js
+++ main.js
@@ -1,31 +1,37 @@
-var curentPlayer = "X"
+"use strict";
 
-var bord = [
-    [null,null,null],
-    [null,null,null],
-    [null,null,null]
-]
+let curentPlayer = "X"
 
-const go = (arrEn, arrTo) => {
-    console.log(bord[arrEn] [arrTo])
+let boardState = new Array(9).fill(true)
+let boardStateOwnd = new Array(9)
+let idForSquare = ['0-0', '0-1', '0-2', '1-0', '1-1', '1-2', '2-0', '2-1', '2-2']
 
+const go = (place) => {
+    if (boardState[place] && !doWeHaveAWinner()){
+        boardState[place] = false;
+        document.getElementById(idForSquare[place]).innerHTML = `<div style="color: white">${curentPlayer}</div>`;
+        boardStateOwnd[place] = curentPlayer;
+        
+        if (doWeHaveAWinner()) {
+            document.getElementById('gmae-info').innerHTML = `<div>Spiller ${curentPlayer} er vinneren &#127881;</div>`;
+        } else {
+            nexstPlayer()
+            document.getElementById('gmae-info').innerHTML = `<div>Det er spiller  ${curentPlayer}  sin tur :)</div>`;
+        }
+    } 
 }
 
-//const ree = (positonin, ) => {}
+document.getElementById('0-0').addEventListener("click", () => go(0));
+document.getElementById('0-1').addEventListener("click", () => go(1));
+document.getElementById('0-2').addEventListener("click", () => go(2));
 
-document.getElementById('0-0').addEventListener("click", () => go([0][0]));
-document.getElementById('0-1').addEventListener("click", () => go([0][1]));
-document.getElementById('0-2').addEventListener("click", () => go([0][2]));
+document.getElementById('1-0').addEventListener("click", () => go(3));
+document.getElementById('1-1').addEventListener("click", () => go(4));
+document.getElementById('1-2').addEventListener("click", () => go(5));
 
-document.getElementById('1-0').addEventListener("click", () => go([1][0]));
-document.getElementById('1-1').addEventListener("click", () => go([1][1]));
-document.getElementById('1-2').addEventListener("click", () => go([1][2]));
-
-document.getElementById('2-0').addEventListener("click", () => go([2][0]));
-document.getElementById('2-1').addEventListener("click", () => go([2][1]));
-document.getElementById('2-2').addEventListener("click", () => go([2][2]));
-
-
+document.getElementById('2-0').addEventListener("click", () => go(6));
+document.getElementById('2-1').addEventListener("click", () => go(7));
+document.getElementById('2-2').addEventListener("click", () => go(8));
 
 const nexstPlayer = () => {
     if (curentPlayer == "X") {
@@ -35,12 +41,15 @@ const nexstPlayer = () => {
     }
 }
 
-const playerImput = () => {
-    
-}
+const doWeHaveAWinner = () => {
+    if (boardStateOwnd[0] == boardStateOwnd[1] && boardStateOwnd[2]) {return true}
+    if (boardStateOwnd[3] == boardStateOwnd[4] && boardStateOwnd[5]) {return true}
+    if (boardStateOwnd[6] == boardStateOwnd[7] && boardStateOwnd[8]) {return true}
 
-const legalMove = () => {
-    if (1==1) {
+    if (boardStateOwnd[0] == boardStateOwnd[3] && boardStateOwnd[6]) {return true}
+    if (boardStateOwnd[1] == boardStateOwnd[4] && boardStateOwnd[7]) {return true}
+    if (boardStateOwnd[2] == boardStateOwnd[5] && boardStateOwnd[8]) {return true}
 
-    }
+    if (boardStateOwnd[0] == boardStateOwnd[4] && boardStateOwnd[8]) {return true}
+    if (boardStateOwnd[2] == boardStateOwnd[4] && boardStateOwnd[6]) {return true}
 }
